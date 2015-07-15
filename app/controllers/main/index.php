5cb530995b8becd0bb12b236576d5a1b2c37d859
Maksa988 <maks.best5@mail.ru>
2015-07-15 18:34:06 +0300

    Update start controller

--- app/controllers/main/index.php
+++ app/controllers/main/index.php
@@ -1,18 +1,16 @@
 <?php
 /*
 * MyUCP
-* File Version 4.0
-* Date: 30.03.2015
+* File Version 4.0.0.1
+* Date: 15.07.2015
 * Developed by Maksa988
 */
 
 class indexController extends Controller {
 	public function index() {
+
+		echo "Hello World!";
 		echo "<pre>";
-		echo "Отправлено письм: 23560";
-		echo "<hr>";
-		echo "Время выполнения задачи: 1.356 сек.";
-		echo "<hr>";
 		print_r($this->config);
 	}
 }

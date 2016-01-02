c5719b62deadce01f6d6a684ef221f9eccb055a7
maksa988 <maks.best5@mail.ru>
2016-01-02 21:07:37 +0200

    Update load functions

--- engine/protected/load.php
+++ engine/protected/load.php
@@ -10,39 +10,49 @@ class Load {
 		$this->registry = $registry;
 	}
 	
-	public function model($name){
-		$modelClass = $name . 'Model';
-		$modelPath = APP_DIR . 'models/' . $name . '.php';
-		
-		if(is_readable($modelPath)){
-			require_once($modelPath);
-			if(class_exists($modelClass)){
-				$this->registry->$modelClass = new $modelClass($this->registry);
-				return true;
+	public function model(){
+		$names = func_get_args();
+		foreach($names[0] as $name){
+			$modelClass = $name . 'Model';
+			$modelPath = APP_DIR . 'models/' . $name . '.php';
+
+			if(is_readable($modelPath)){
+				require_once($modelPath);
+				if(class_exists($modelClass)){
+					$this->registry->$modelClass = new $modelClass($this->registry);
+				}
+			} else {
+				new Debug('Ошибка: Не удалось загрузить модель ' . $name . '!');
 			}
 		}
-		new Debug('Ошибка: Не удалось загрузить модель ' . $name . '!');
+
+		return true;
 	}
 	
-	public function library($name){
-		$libClass = $name . 'Library';
-		$libPath = ENGINE_DIR . 'lib/' . $name . '.php';
-		
-		if(is_readable($libPath)){
-			require_once($libPath);
-			return true;
+	public function library(){
+		$names = func_get_args();
+		foreach($names[0] as $name){
+			$libClass = $name . 'Library';
+			$libPath = ENGINE_DIR . 'lib/' . $name . '.php';
+			
+			if(is_readable($libPath)){
+				require_once($libPath);
+			} else {
+				new Debug('Ошибка: Не удалось загрузить библиотеку ' . $name . '!');
+			}
 		}
-		new Debug('Ошибка: Не удалось загрузить библиотеку ' . $name . '!');
+
+		return true;
 	}
 }
 
 
-function model($name){
+function model(){
 	global $registry;
-	return $registry->load->model($name);
+	return $registry->load->model(func_get_args());
 }
 
-function library($name){
+function library(){
 	global $registry;
-	return $registry->load->library($name);
+	return $registry->load->library(func_get_args());
 }

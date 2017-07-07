ab2c0415165c336d8d2a99bd85ef31bddc28682e
maksa988 <maks.best5@mail.ru>
2017-07-07 23:03:33 +0300

    fix routes

--- app/routers.php
+++ app/routers.php
@@ -8,9 +8,7 @@
 | с их параметрами и так же действиями которые они должны выполнять
 |
 */
-//
-//Router::any("/", function() {
-//	return view("welcome");
-//});
 
-Router::any("/", "HomeController@welcome");
+Router::any("/", function() {
+	return view("welcome");
+});

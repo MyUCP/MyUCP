764410ba31c7de7d7610fe752d3dfabb8c1879ad
maksa988 <maks.best5@mail.ru>
2018-04-16 00:29:17 +0300

    fix

--- engine/protected/Routing/UrlGenerator.php
+++ engine/protected/Routing/UrlGenerator.php
@@ -10,6 +10,13 @@ class UrlGenerator
     protected $routes;
 
     /**
+     * The Request instance
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
      * UrlGenerator constructor.
      *
      * @param RouteCollection $routes

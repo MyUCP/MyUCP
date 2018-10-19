84e3b9ae30da515c92b51d9354ec4650710779ec
maksa988 <maksa988ua@gmail.com>
2018-10-19 16:27:49 +0300

    remove psr4 paths

--- configs/autoload_classes.php
+++ configs/autoload_classes.php
@@ -9,12 +9,5 @@
 */
 
 return [
-
-    // Extensions
-    \Extensions\ExampleBoot\ExampleBoot::class => EXTENSIONS_DIR . 'ExampleBoot/ExampleBoot.php',
-    \Extensions\Example\Example::class => EXTENSIONS_DIR . 'Example/Example.php',
-
     //
-    \Extensions\Validate\Validate::class => EXTENSIONS_DIR . 'Validate/Validate.php',
-    \Extensions\Auth\Auth::class => EXTENSIONS_DIR . 'Auth/Auth.php',
 ];
